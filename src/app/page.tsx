857eb8a720ebbb563d1e245e8b02d713f577984b
Folkert-Jan van der Pol <folkert-jan@buildinamsterdam.com>
2024-01-03 15:50:31 +0100

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>I am a button</Button>
+    </main>
   )
 }
 
